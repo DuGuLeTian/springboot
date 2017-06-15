561f8175b529ed1a1187d97b7a2e6b439c797272
DuGuLeTian <lth1429081529@gmail.com>
2017-06-15 22:38:08 +0800

    fix

---
+++ src/main/java/com/duguletian/repository/ReaderRepository.java
@@ -0,0 +1,10 @@
+package com.duguletian.repository;
+
+import com.duguletian.module.Reader;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by lth on 2017/6/15.
+ */
+public interface ReaderRepository extends JpaRepository<Reader, String>{
+}
